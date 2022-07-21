62d09b519b1c180b6cfb58367411153ab211b233
ThisuraSagara <thisuras4@gmail.com>
2022-07-21 22:47:25 +0530

    Update Change

--- src/main/java/com/javajap/japsqldemo/entity/Product.java
+++ src/main/java/com/javajap/japsqldemo/entity/Product.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -16,17 +18,29 @@ public class Product {
 
     @Id
     @GeneratedValue
+    @NotNull
     private int id;
 
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
     private Date createdDate = new Date();
 
     @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
     private Date updatedDate = new Date();
 
+    @NotNull
     private String name;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
     private int quantity;
+
+    @NotNull
     private double price;
 
 }
